f38f71f5b8a8bb5e8c98b8da630a0b21c60fb976
tipacar <egetipacar@gmail.com>
2024-06-10 21:04:29 +0300

    Add files via upload

---
+++ discord_bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from bot_token import token
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run(token)
