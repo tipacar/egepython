09908dc75f8d1ca053b8807193303ad8ca7a3b1b
tipacar <egetipacar@gmail.com>
2024-06-22 13:42:08 +0300

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -23,4 +23,16 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
 bot.run(token)
